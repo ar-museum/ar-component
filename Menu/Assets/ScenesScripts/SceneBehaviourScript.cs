db682c3cf9cdd5634d58ffa6380828f3439e8c84
Cristian Adam <48415468+adamcristi@users.noreply.github.com>
2019-03-31 16:17:59 +0300

    Add files via upload

---
+++ Menu/Assets/ScenesScripts/SceneBehaviourScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SceneBehaviourScript : MonoBehaviour
+{
+    Scene currentScene;
+
+    void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Application.platform == RuntimePlatform.Android)
+        {
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                string sceneName = currentScene.name;
+                if (string.Compare(sceneName, "ARScene") == 0 || string.Compare(sceneName, "GalleryScene") == 0 || string.Compare(sceneName, "GamesScene") == 0)
+                {
+                    SceneManager.LoadScene("MenuScene");
+                }
+                else
+                {
+                    Application.Quit();
+                }
+            }
+        }
+    }
+
+    public void LoadScene(string sceneCamera)
+    {
+        SceneManager.LoadScene(sceneCamera);
+    }
+}
