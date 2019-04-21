0fe804b32eb09ab945e0011199cfeab2b7ac1072
Lipan Matei <lipanmatei@gmail.com>
2019-04-21 23:14:25 +0300

    Added CleanScreenAttachedInfo function and its test

--- Menu_And_AR/Assets/Scripts/AR_TEAM/MyImageTargetTrackableEventHandler.cs
+++ Menu_And_AR/Assets/Scripts/AR_TEAM/MyImageTargetTrackableEventHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MyImageTargetTrackableEventHandler : DefaultTrackableEventHandler
 {
@@ -18,15 +19,16 @@ public class MyImageTargetTrackableEventHandler : DefaultTrackableEventHandler
     {
         base.OnTrackingLost();
         ShowARUIFrame();
+        CleanScreenAttachedInfo();
         ARDisplayTypeSwitcher.HideScreenAttachedComponents(ARDisplayTypeSwitcher.getScreenAttachedObjects());
     }
 
-    public void HideARUIFrame()
+    private void HideARUIFrame()
     {
         GameObject.FindGameObjectWithTag("ARUIFrame").transform.localScale = new Vector3(0, 0, 0);
     }
 
-    public void ShowARUIFrame()
+    private void ShowARUIFrame()
     {
         GameObject.FindGameObjectWithTag("ARUIFrame").transform.localScale = new Vector3(1, 1, 1);
     }
@@ -37,8 +39,41 @@ public class MyImageTargetTrackableEventHandler : DefaultTrackableEventHandler
         var targetName = mTrackableBehaviour.TrackableName;
         foreach (var screenAttachedComponent in screenAttachedComponents)
         {
+            //show text
             var screenAttachedTexts = screenAttachedComponent.GetComponentsInChildren<SetText>();
+            foreach (var screenAttachedText in screenAttachedTexts)
+            {
+                screenAttachedText.transform.localScale = new Vector3(1, 4, 1);
+            }
+            // update text
             TargetManager.SetInfoForTextComponents(screenAttachedTexts, targetName);
+            //show button
+            var screenAttachedButtons = screenAttachedComponent.GetComponentsInChildren<Button>();
+            foreach(var screenAttachedButton in screenAttachedButtons)
+            {
+                screenAttachedButton.transform.localScale = new Vector3(1, 10, 1);
+            }
+        }
+    }
+
+    public void CleanScreenAttachedInfo()
+    {
+        var screenAttachedComponents = ARDisplayTypeSwitcher.getScreenAttachedObjects();
+        var targetName = mTrackableBehaviour.TrackableName;
+        foreach (var screenAttachedComponent in screenAttachedComponents)
+        {
+            //hide text
+            var screenAttachedTexts = screenAttachedComponent.GetComponentsInChildren<SetText>();
+            foreach( var screenAttachedText in screenAttachedTexts)
+            {
+                screenAttachedText.transform.localScale = new Vector3(0,0,0);
+            }
+            //hide button
+            var screenAttachedButtons = screenAttachedComponent.GetComponentsInChildren<Button>();
+            foreach (var screenAttachedButton in screenAttachedButtons)
+            {
+                screenAttachedButton.transform.localScale = new Vector3(0,0,0);
+            }
         }
     }
 
--- Menu_And_AR/Assets/Tests/MyImageTargetTrackableEventHandlerTest.cs
+++ Menu_And_AR/Assets/Tests/MyImageTargetTrackableEventHandlerTest.cs
@@ -7,9 +7,9 @@ using UnityEngine.SceneManagement;
 
 namespace Tests
 {
-    // test 1 Matei Lipan
     public class MyImageTargetTrackableEventHandlerTest
     {
+        // test 1 Matei Lipan
         [UnityTest]
         public IEnumerator MyImageTargetTrackableEventHandler_UpdateScreenAttachedInfo_Test()
         {
@@ -27,6 +27,30 @@ namespace Tests
                 if (setText.textType == SetText.TextType.TopText)
                 {
                     Assert.AreEqual(imageTargetTrackableEventHandler.GetTrackableName(), setText.GetText());
+                }
+            }
+        }
+
+        // test 2 Matei Lipan
+        [UnityTest]
+        public IEnumerator MyImageTargetTrackableEventHandler_CleanScreenAttachedInfo_Test()
+        {
+            //Arrange
+            SceneManager.LoadScene("ARScene");
+            yield return new WaitForSeconds(1);
+
+            //Act
+            var imageTargetTrackableEventHandler = GameObject.FindGameObjectWithTag("TargetManager").GetComponentInChildren<MyImageTargetTrackableEventHandler>();
+            imageTargetTrackableEventHandler.CleanScreenAttachedInfo();
+            var screenAttachedObjects =  ARDisplayTypeSwitcher.getScreenAttachedObjects();
+
+            //Assert
+            foreach (var screenAttachedObject in screenAttachedObjects)
+            {
+                var childrenTransform = screenAttachedObject.GetComponentsInChildren<Transform>();
+                foreach( var childTransform in childrenTransform)
+                {
+                    Assert.IsTrue(childTransform.localScale == new Vector3(0, 0, 0));
                 }
             }
         }
