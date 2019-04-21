f98e41436cab2056a2a2f515d39157ffdc91870d
stioana79 <ioanadana97@gmail.com>
2019-04-22 00:13:29 +0300

    Updated scene and added background images Updated the script so that the images will update based on the location

--- Menu_And_AR/Assets/Scripts/AR_TEAM/LocationBehaviourScript.cs
+++ Menu_And_AR/Assets/Scripts/AR_TEAM/LocationBehaviourScript.cs
@@ -2,105 +2,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
 using UnityEngine.UI;
-using UnityEngine.Android;
-using UnityEngine.SceneManagement;
 
 public class LocationBehaviourScript : MonoBehaviour
 {
 
-    public GameObject text0;
-    public GameObject text1;
-    public GameObject text2;
-    public GameObject text3;
-
-    static double latitudine = 0;
-    static double longitudine = 0;
-
-    ArrayList museumsList = new ArrayList();
-    List<GameObject> textObjectList = new List<GameObject>();
-
-    void Awake()
+    public GameObject img1;
+    public GameObject img2;
+    public GameObject img3;
+    public GameObject backroundMuzeulLiteraturi;
+    public GameObject backroundMuzeulUnirii;
+    public GameObject backroundNoMuzeum;
+    double latitudine = 0;
+    double longitudine = 0;
+    
+    void Start()
     {
-        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
-        {
-            Permission.RequestUserPermission(Permission.FineLocation);
-        }
-
-        textObjectList.Add(text0);
-        textObjectList.Add(text1);
-        textObjectList.Add(text2);
-        textObjectList.Add(text3);
-
-        text0.SetActive(false);
-        text1.SetActive(false);
-        text2.SetActive(false);
-        text3.SetActive(false);
-
-        museumsList.Add(new Museum("Muzeul de Literatura", 47.172032, 27.576216, 1));
-        museumsList.Add(new Museum("Muzeul Unirii", 47.167430, 27.578895, 2));
-
+        img1.SetActive(false);
+        img2.SetActive(false);
+        img3.SetActive(false);
+        backroundMuzeulLiteraturi.SetActive(false);
+        backroundMuzeulUnirii.SetActive(false);
+        backroundNoMuzeum.SetActive(false);
+        StartCoroutine(StartLocationService());
     }
 
-    IEnumerator Start()
+    IEnumerator StartLocationService()
     {
-        if (Permission.HasUserAuthorizedPermission(Permission.FineLocation)) // aplicatia are acces la locatie
-        {
-                if (latitudine == 0 && longitudine == 0)
-                {
-                    yield return StartCoroutine(LocationService());
-                }
-                else
-                {
-                    yield return StartCoroutine(SetText());
-                }
-            }
-        else  // aplicatia nu are acces la locatie
-        {
-            textObjectList[0].SetActive(true);
-        }
-    }
-
-
-    IEnumerator SetText()
-    {
-        bool gasit = false;
-        if (Permission.HasUserAuthorizedPermission(Permission.FineLocation))
-        {
-            foreach (Museum muzeu in museumsList)
-            {
-                if (Math.Abs(latitudine - muzeu.getLatitude()) < 0.00001 && Math.Abs(longitudine - muzeu.getLongitude()) < 0.00001)
-                {
-                    string tagCautat = "Text" + muzeu.getTextNumber();
-
-                    foreach(GameObject obiectText in textObjectList)
-                    {
-                        if (obiectText.name.Equals(tagCautat))
-                        {
-                            obiectText.SetActive(true);
-                            gasit = true;
-                        }
-                    }
-                }
-            }
-            if (!gasit) // alta locatie
-            {
-                textObjectList[1].SetActive(true);
-            }
-        }
-        yield return null;
-    }
-
-    IEnumerator LocationService()
-    {
-
         if (!Input.location.isEnabledByUser)
         {
             print("Utlizatorul nu a activat GPS-ul.");
-            latitudine = -1;
-            longitudine = -1;
-            yield return StartCoroutine(SetText());
             yield break;
         }
 
@@ -116,37 +47,61 @@ public class LocationBehaviourScript : MonoBehaviour
         if (maxWait < 1)
         {
             print("Timpul a expirat.");
-            latitudine = -1;
-            longitudine = -1;
-            yield return StartCoroutine(SetText());
             yield break;
         }
 
         if (Input.location.status == LocationServiceStatus.Failed)
         {
             print("Nu s-a putut determina locatia device-ului.");
-            latitudine = -1;
-            longitudine = -1;
-            yield return StartCoroutine(SetText());
             yield break;
         }
         else
         {
             latitudine = Input.location.lastData.latitude;
             longitudine = Input.location.lastData.longitude;
-            yield return StartCoroutine(SetText());
         }
 
         Input.location.Stop();
     }
 
+    void Update()
+    {
+        if (Math.Abs(latitudine - 47.172032) < 0.00001 && Math.Abs(longitudine - 27.576216) < 0.00001) // Muzeul de Literatura
+        {
+            img1.SetActive(false);
+            img2.SetActive(true);
+            img3.SetActive(false);
+            backroundMuzeulLiteraturi.SetActive(true);
+            backroundMuzeulUnirii.SetActive(false);
+            backroundNoMuzeum.SetActive(false);
+        }
+        else if (Math.Abs(latitudine - 47.167430) < 0.00001 && Math.Abs(longitudine - 27.578895) < 0.00001) // Muzeul Unirii
+        {
+            img1.SetActive(false);
+            img2.SetActive(false);
+            img3.SetActive(true);
+            backroundMuzeulLiteraturi.SetActive(false);
+            backroundMuzeulUnirii.SetActive(true);
+            backroundNoMuzeum.SetActive(false);
+        }
+        else
+        {
+            img1.SetActive(true);
+            img2.SetActive(false);
+            img3.SetActive(false);
+            backroundMuzeulLiteraturi.SetActive(false);
+            backroundMuzeulUnirii.SetActive(false);
+            backroundNoMuzeum.SetActive(true);
+        }
+    }
+
     public double getLatitude()
     {
-        return latitudine;
+        return this.latitudine;
     }
 
     public double getLongitude()
     {
-        return longitudine;
+        return this.longitudine;
     }
 }
