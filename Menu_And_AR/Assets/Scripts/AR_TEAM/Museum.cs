7c0f36acccc529199f4fd90430080c8e3335cedd
adamcristi <48415468+adamcristi@users.noreply.github.com>
2019-04-21 23:33:54 +0300

    Improve location detection

---
+++ Menu_And_AR/Assets/Scripts/AR_TEAM/Museum.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Museum
+{
+    private string museumName;
+    private double latitude;
+    private double longitude;
+    private int textNumber;
+
+    public Museum(string numeMuzeu, double latitudine, double longitudine, int numarText)
+    {
+        this.museumName = numeMuzeu;
+        this.latitude = latitudine;
+        this.longitude = longitudine;
+        this.textNumber = numarText + 1;
+    }
+
+    public string getMuseumName()
+    {
+        return this.museumName;
+    }
+
+    public double getLatitude()
+    {
+        return this.latitude;
+    }
+
+    public double getLongitude()
+    {
+        return this.longitude;
+    }
+
+    public int getTextNumber()
+    {
+        return this.textNumber;
+    }
+}
