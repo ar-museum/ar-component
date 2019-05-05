acbb9ff6760c12b6171ad2a99e49b13096e3e322
Andrei <andreidaamian@gmail.com>
2019-05-05 19:32:19 +0300

    Authors deserialization

---
+++ Menu_And_AR/Assets/Scripts/AR_TEAM/Http/Author.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Assets.Scripts.AR_TEAM.Http {
+    class Author {
+        public int AuthorId { get; set; }
+        public string FullName { get; set; }
+        public int BornYear { get; set; }
+        public int DiedYear { get; set; }
+        public string Location { get; set; }
+        public int PhotoId { get; set; }
+        public int StaffId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
