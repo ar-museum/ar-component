13b2ad0e07419e5ab97f6b5fd6564c5e35b8127d
Andrei <andreidaamian@gmail.com>
2019-05-07 02:08:35 +0300

    Dynamic loading vuforia database

--- Menu_And_AR/Assets/Scripts/AR_TEAM/Scripts for ARScene/TargetManager.cs
+++ Menu_And_AR/Assets/Scripts/AR_TEAM/Scripts for ARScene/TargetManager.cs
@@ -2,6 +2,9 @@
 using UnityEngine;
 using Vuforia;
 using System.Linq;
+using System.IO;
+using UnityEditor;
+using System;
 
 public class TargetManager : MonoBehaviour
 {
@@ -13,26 +16,94 @@ public class TargetManager : MonoBehaviour
     private void Awake()
     {
         // After all from Vuforia is loaded, call my function
-        VuforiaARController.Instance.RegisterVuforiaStartedCallback(DoAfterVuforiaStarted);
+        VuforiaARController.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
     }
 
     private void OnDestroy()
     {
         // Unregister my function from being called 
-        VuforiaARController.Instance.UnregisterVuforiaStartedCallback(DoAfterVuforiaStarted);
+        VuforiaARController.Instance.UnregisterVuforiaStartedCallback(OnVuforiaStarted);
     }
 
     // TargetManagerTests test 3
-    private void DoAfterVuforiaStarted()
-    {
-        // Load database
-        LoadDatabase(CurrentVuforiaDatabase);
+    void OnVuforiaStarted() {
 
-        // Get trackable targers
-        Targets = GetTargets();
+        // The 'path' string determines the location of xml file
+        // For convinence the RealTime.xml is placed in the StreamingAssets folder
+        // This file can be downloaded and the relative path will be used accordingly
 
-        // Setup targets
-        SetupTargets(Targets, ImageTargetChildPrefab);
+        //Debug.Log("App path: " + Application.dataPath);
+        //File.WriteAllText("output.txt", "App path: " + Application.dataPath);
+
+        var androidPath = "/storage/emulated/0/vufo/ARMuseum.xml";
+        var androiddddd = Application.persistentDataPath + "/ARMuseum.xml";
+
+        Directory.CreateDirectory(Application.persistentDataPath);
+        File.WriteAllText(Application.persistentDataPath + "/output.txt", "App path: " + Application.dataPath
+            + "\n" + "sdcard: " + Application.persistentDataPath + "\nfff: " + androidPath
+            + "\nxxx: " + androiddddd);
+        File.AppendAllText(Application.persistentDataPath + "/output.txt", "\n" + File.ReadAllText(androiddddd));
+
+
+        string path = "";
+#if UNITY_IPHONE
+		path = Application.dataPath + "/Raw/RealTime.xml";
+#elif UNITY_ANDROID
+        path = androiddddd; //"jar:file://" + Application.dataPath + "!/assets/RealTime.xml";
+#else
+		path = Application.dataPath + "/StreamingAssets/Vuforia/testy/StreamingAssets/ARMuseum.xml";
+#endif
+
+        bool status = false;
+        try {
+            status = LoadDataSet(path, VuforiaUnity.StorageType.STORAGE_ABSOLUTE);
+        } catch (Exception e) {
+            File.AppendAllText(Application.persistentDataPath + "/output.txt", "\nexception: " + e.ToString());
+        }
+        File.AppendAllText(Application.persistentDataPath + "/output.txt", "\nstatus: " + status.ToString());
+
+
+        if (status) {
+            Debug.Log("Dataset Loaded");
+        } else {
+            Debug.Log("Dataset Load Failed");
+        }
+
+        SetupTargets(GetTargets(), ImageTargetChildPrefab);
+    }
+
+    // Load and activate a data set at the given path.
+    private bool LoadDataSet(string dataSetPath, VuforiaUnity.StorageType storageType) {
+        // Request an ImageTracker instance from the TrackerManager.
+        ObjectTracker objectTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+
+        objectTracker.Stop();
+        IEnumerable<DataSet> dataSetList = objectTracker.GetActiveDataSets();
+        foreach (DataSet set in dataSetList.ToList()) {
+            objectTracker.DeactivateDataSet(set);
+        }
+
+        // Check if the data set exists at the given path.
+        if (!DataSet.Exists(dataSetPath, storageType)) {
+            Debug.LogError("Data set " + dataSetPath + " does not exist.");
+            return false;
+        }
+
+        // Create a new empty data set.
+        DataSet dataSet = objectTracker.CreateDataSet();
+
+        // Load the data set from the given path.
+        if (!dataSet.Load(dataSetPath, storageType)) {
+            Debug.LogError("Failed to load data set " + dataSetPath + ".");
+            return false;
+        }
+
+        // (Optional) Activate the data set.
+        objectTracker.ActivateDataSet(dataSet);
+        objectTracker.Start();
+
+
+        return true;
     }
 
     // TargetManagerTests test 1 test 2 test 3
