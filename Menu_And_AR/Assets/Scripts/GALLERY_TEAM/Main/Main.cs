7f3c48baac462f9d815aff44789e7483a243baf2
mariusBlaj47 <47899839+mariusBlaj47@users.noreply.github.com>
2019-05-21 11:07:52 +0300

    "Museum name"

--- Menu_And_AR/Assets/Scripts/GALLERY_TEAM/Main/Main.cs
+++ Menu_And_AR/Assets/Scripts/GALLERY_TEAM/Main/Main.cs
@@ -10,8 +10,10 @@ public class Main : MonoBehaviour
     public GameObject buttonPrefab;
     public GameObject panelToAttachButtonsTo;
     public GameObject panelToAttachButtonsTo2;
+    [SerializeField] private Text txtTitle;
     void Start()
     {
+        txtTitle.text = MuseumManager.Instance.CurrentMuseum.Name;
         int count = 0;
         foreach (var author in MuseumManager.Instance.CurrentMuseum.Authors)
         {
@@ -36,7 +38,7 @@ public class Main : MonoBehaviour
         button.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
         button.GetComponent<Button>().onClick.AddListener(delegate { OnClick(index); });
 
-        string das,das2, imagePath;
+        string das, das2, imagePath;
         int born, died;
 
         (das2, born, died, das, imagePath) = MuseumManager.Instance.CurrentMuseum.GetAuthorDataById(index);
@@ -61,8 +63,8 @@ public class Main : MonoBehaviour
         button.GetComponent<RectTransform>().localScale = new Vector2(1, 1);
         button.GetComponent<Button>().onClick.AddListener(delegate { OnClick2(index); });
 
-        string das, das2, das3,das4, imagePath;
-        (das3, das, das2, imagePath,das4) = MuseumManager.Instance.CurrentMuseum.GetExhibitDataById(index);
+        string das, das2, das3, das4, imagePath;
+        (das3, das, das2, imagePath, das4) = MuseumManager.Instance.CurrentMuseum.GetExhibitDataById(index);
 
         byte[] byteArray = File.ReadAllBytes(imagePath);
         Texture2D texture = new Texture2D(8, 8);
